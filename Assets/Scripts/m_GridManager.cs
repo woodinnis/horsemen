d652b267d366d0907174d7c3f228b32d50ec17c1
wood.innis <wood.innis@c2d2af7e-3148-0f7d-02ac-1799aeb7fff9>
2014-09-30 20:29:53 +0000

    Second-level "Grid" parent renamed to "Field" Hex manager applied to all hexes in field "Hex" tag applied to all Hexes in "Field"

---
+++ Assets/Scripts/m_GridManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class m_GridManager : MonoBehaviour {
+
+	public new GameObject[] Hex;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/Scripts/u_Movement.cs
+++ Assets/Scripts/u_Movement.cs
@@ -3,29 +3,45 @@ using System.Collections;
 
 public class u_Movement : MonoBehaviour {
 
+	/*
 	// Variables for checking valid hex locations
-	bool boolIsEmpty = false;
-	bool boolIsEnemy = false;
-	bool boolIsAlly = false;
+	public int[][] HexStatus;
 
-	// Variables for direction and movement
-	int intMoveHexCount = 0;
-	int intMoveHexDirection = 0;
-	int intMoveHexRemaining = 0;
-	string strCurrentHex;
-
+	private boolIsEmpty = false;
 
 	// Use this for initialization
 	void Start () 
 	{
-		//strCurrentHex = gameObject.GetComponent<"Hex_01">();
+
+	}
+
+	void Update()
+	{
+		playerLocation = OnMouseDown();
+
+		UnitMove(playerLocation);
 	}
 
 	// Calculate unit movements
-	void UnitMove()
-	{}
+	void UnitMove(Vector3 moveTo)
+	{
+		float movementX = speedX * Time.deltaTime;
+		float movementZ = speedZ * Time.deltaTime;
+
+		playerMove = new Vector3(movementX, 0.0f, movementZ);
+
+
+	}
+
+	Vector3 OnMouseDown()
+	{
+		Vector3 target = Input.mousePosition;
+
+		return target;
+	}
 
 	// Actions to perform on unit stop
 	//bool UnitStop()
 	//{}
+	*/
 }
