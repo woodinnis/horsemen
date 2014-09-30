4efc065ed1335c6cb50623632e2f540680aa56ee
kamiakase14@gmail.com <kamiakase14@gmail.com@c2d2af7e-3148-0f7d-02ac-1799aeb7fff9>
2014-09-30 19:53:03 +0000

    [Horsemen]
    - Units with script
    - Unit Controller
    - Unit Attack
    - Game Manager

---
+++ Assets/Scripts/m_GameManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class m_GameManager : MonoBehaviour
+{
+
+	public int playerLevel;
+	public int playerExp;
+
+	//some variables for the future skill tree HERE
+}
