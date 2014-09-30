e2ab08eb9c333de5ad6b8c4d0da5c766d7e641d1
kamiakase14@gmail.com <kamiakase14@gmail.com@c2d2af7e-3148-0f7d-02ac-1799aeb7fff9>
2014-09-30 20:10:05 +0000

    [Horsemen]
    - Hex Controller

---
+++ Assets/Scripts/g_HexController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class g_HexController : MonoBehaviour
+{
+	public bool isEnemy; // My child is an enemy
+	public bool isAlly; // My child is an ally
+	public bool isEmpty; // I do not have a child
+
+	public GameObject child; // If I do have a child, this is who it is
+
+	void Start()
+	{
+		isEnemy = false;
+		isAlly = false;
+		isEmpty = true;
+	}
+}
