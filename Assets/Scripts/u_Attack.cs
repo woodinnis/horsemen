158c69f6c5d79ba9a89a34298b5da58b2d1183e7
kamiakase14@gmail.com <kamiakase14@gmail.com@c2d2af7e-3148-0f7d-02ac-1799aeb7fff9>
2014-09-30 00:30:54 +0000

    [Horsemen]
    - Unit manager

--- Assets/Scripts/u_Attack.cs
+++ Assets/Scripts/u_Attack.cs
@@ -3,16 +3,13 @@ using System.Collections;
 
 public class u_Attack : MonoBehaviour
 {
-	public bool hit;
-
 	void Start ()
 	{
 		if (Random.Range (1, 7) >= 3)
 		{
-			hit = true;
-			// other.TakeDamage(weapon_dmg);
-			hit = false;
-			//m_UnitManager.instance.attacked = true;
+			// other.TakeDamage(unit_dmg);
+			//m_UnitManager.instance.iAttacked = true;
+			Debug.Log("Damage taken");
 		}
 	}
 }
