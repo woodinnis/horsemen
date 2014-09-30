2114f31d6b0ea9fe65b80d11470a87ad62e255ba
kamiakase14@gmail.com <kamiakase14@gmail.com@c2d2af7e-3148-0f7d-02ac-1799aeb7fff9>
2014-09-30 00:30:54 +0000

    [Horsemen]
    - Unit manager

---
+++ trunk/Assets/Scripts/m_UnitManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class m_UnitManager : MonoBehaviour
+{
+	[HideInInspector]
+	public static m_UnitManager instance; // static allows access from anywhere
+	
+	void Awake()
+	{
+		instance = this;
+	}
+	
+	void Start ()
+	{
+	
+	}
+
+	void Update ()
+	{
+	
+	}
+
+	void EndTurn() // Check to see if turn should be ended or not
+	{
+
+	}
+}
