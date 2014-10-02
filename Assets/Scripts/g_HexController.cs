ece4d66e2e85d6a1c5b853709b4cff7dae9afc91
wood.innis <wood.innis@c2d2af7e-3148-0f7d-02ac-1799aeb7fff9>
2014-10-02 20:18:32 +0000

    Made m_GridManager a prefab g_HexController.cs commented out (See comments) u_Movement.cs commented out (See comments)

--- Assets/Scripts/g_HexController.cs
+++ Assets/Scripts/g_HexController.cs
@@ -3,11 +3,24 @@ using System.Collections;
 
 public class g_HexController : MonoBehaviour
 {
+	/*
+	 * 
+	 * This class can and should be a part of m_GridManager.cs
+	 * It will be systematically transitioned there over the next few days 
+	 * 
+	 * g_HexController.cs will be kept and possibly repurposed in some way
+	 *  
+	 * 
+	public Transform occupantTransform;
+	public string occupantName;
+
 	public bool isEnemy; // My child is an enemy
 	public bool isAlly; // My child is an ally
 	public bool isEmpty; // I do not have a child
 
 	public GameObject child; // If I do have a child, this is who it is
+
+	private 
 
 	void Start()
 	{
@@ -15,4 +28,13 @@ public class g_HexController : MonoBehaviour
 		isAlly = false;
 		isEmpty = true;
 	}
+
+	void Update()
+	{
+		if(Input.GetMouseButtonUp)
+		{
+			m_GridManager(
+		}
+	}
+	*/
 }
--- Assets/Scripts/u_Movement.cs
+++ Assets/Scripts/u_Movement.cs
@@ -4,22 +4,30 @@ using System.Collections;
 public class u_Movement : MonoBehaviour {
 
 	/*
-	// Variables for checking valid hex locations
-	public int[][] HexStatus;
+	 * 
+	 * Finishing u_Movement.cs will require completing the merging of g_HexController.cs with m_GridManager.cs
+	 * 
+	 * When all Hex details are accessable from one Singleton, a movement engine can be built.
+	 * 
+	// Movement speed variables for player/unit
+	public float speedX = 0.0f;
+	public float speedZ = 0.0f;
 
-	private boolIsEmpty = false;
+	// Current and Target Hex location variables
+	private GameObject hexCurrent;
+	private GameObject hexTarget;
+
+
 
 	// Use this for initialization
 	void Start () 
 	{
-
+		hexCurrent =
 	}
 
 	void Update()
 	{
-		playerLocation = OnMouseDown();
 
-		UnitMove(playerLocation);
 	}
 
 	// Calculate unit movements
@@ -27,21 +35,6 @@ public class u_Movement : MonoBehaviour {
 	{
 		float movementX = speedX * Time.deltaTime;
 		float movementZ = speedZ * Time.deltaTime;
-
-		playerMove = new Vector3(movementX, 0.0f, movementZ);
-
-
 	}
-
-	Vector3 OnMouseDown()
-	{
-		Vector3 target = Input.mousePosition;
-
-		return target;
-	}
-
-	// Actions to perform on unit stop
-	//bool UnitStop()
-	//{}
 	*/
 }
